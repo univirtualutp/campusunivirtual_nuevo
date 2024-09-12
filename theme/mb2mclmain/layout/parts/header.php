fe0beb1a2f2702d5cb0985b1a1054d5df150f2c0
arenasdaniel <daniel.pardo@utp.edu.co>
2024-09-12 17:02:35 -0500

    se agrega texto para ingreso

--- theme/mb2mclmain/layout/parts/header.php
+++ theme/mb2mclmain/layout/parts/header.php
@@ -84,6 +84,8 @@ $menuPos = 1;
 								<div class="header-tools">
 									<?php echo theme_mb2mclmain_panel_links(); ?>
 									<a href="#" class="tools-close"><i class="lni-close"></i></a>
+									<!-- Aquí se agrega el texto -->
+									<span class="custom-text">Ingresa acá</span>
 								</div>
 								<div class="tools-show">
 									<div class="tools-show-inner">
